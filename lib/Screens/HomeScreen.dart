125b240e2859888f04aa50e0ce1d1039cae0562d
namld <17520777@gm.uit.edu.vn>
2020-12-06 11:03:13 +0700

    Update HomeScreen.dart

--- lib/Screens/HomeScreen.dart
+++ lib/Screens/HomeScreen.dart
@@ -1,9 +1,255 @@
 import 'package:flutter/material.dart';
-class HomeScreen extends StatelessWidget{
+import 'package:flutter/rendering.dart';
+import 'dart:math' as math;
+import '../Utils/Contants.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: Constants.appName,
+      theme: Constants.lightTheme,
+      darkTheme: Constants.darkTheme,
+      home: HomeScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  PageController _pageController;
+  int _page = 0;
+  List navs = ["Trang chủ", "Cộng đồng", "Yêu thích"];
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Business',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('BottomNavigationBar Sample'),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
 }
+
+class SlidingCardsView extends StatefulWidget {
+  @override
+  _SlidingCardsViewState createState() => _SlidingCardsViewState();
+}
+
+class _SlidingCardsViewState extends State<SlidingCardsView> {
+  PageController pageController;
+  double pageOffset = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    pageController = PageController(viewportFraction: 0.8);
+    pageController.addListener(() {
+      setState(() => pageOffset = pageController.page);
+    });
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.55,
+      child: PageView(
+        controller: pageController,
+        children: <Widget>[
+          SlidingCard(
+            name: 'Shenzhen GLOBAL DESIGN AWARD 2018',
+            date: '4.20-30',
+            assetName: 'steve-johnson.jpeg',
+            offset: pageOffset,
+          ),
+          SlidingCard(
+            name: 'Dawan District, Guangdong Hong Kong and Macao',
+            date: '4.28-31',
+            assetName: 'rodion-kutsaev.jpeg',
+            offset: pageOffset - 1,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SlidingCard extends StatelessWidget {
+  final String name;
+  final String date;
+  final String assetName;
+  final double offset;
+
+  const SlidingCard({
+    Key key,
+    @required this.name,
+    @required this.date,
+    @required this.assetName,
+    @required this.offset,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double gauss = math.exp(-(math.pow((offset.abs() - 0.5), 2) / 0.08));
+    return Transform.translate(
+      offset: Offset(-32 * gauss * offset.sign, 0),
+      child: Card(
+        margin: EdgeInsets.only(left: 8, right: 8, bottom: 24),
+        elevation: 8,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(32)),
+        child: Column(
+          children: <Widget>[
+            ClipRRect(
+              borderRadius: BorderRadius.vertical(top: Radius.circular(32)),
+              child: Image.asset(
+                'assets/$assetName',
+                height: MediaQuery.of(context).size.height * 0.3,
+                alignment: Alignment(-offset.abs(), 0),
+                fit: BoxFit.none,
+              ),
+            ),
+            SizedBox(height: 8),
+            Expanded(
+              child: CardContent(
+                name: name,
+                date: date,
+                offset: gauss,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CardContent extends StatelessWidget {
+  final String name;
+  final String date;
+  final double offset;
+
+  const CardContent(
+      {Key key,
+        @required this.name,
+        @required this.date,
+        @required this.offset})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Transform.translate(
+            offset: Offset(8 * offset, 0),
+            child: Text(name, style: TextStyle(fontSize: 20)),
+          ),
+          SizedBox(height: 8),
+          Transform.translate(
+            offset: Offset(32 * offset, 0),
+            child: Text(
+              date,
+              style: TextStyle(color: Colors.grey),
+            ),
+          ),
+          Spacer(),
+          Row(
+            children: <Widget>[
+              Transform.translate(
+                offset: Offset(48 * offset, 0),
+                child: RaisedButton(
+                  color: Color(0xFF162A49),
+                  child: Transform.translate(
+                    offset: Offset(24 * offset, 0),
+                    child: Text('Reserve'),
+                  ),
+                  textColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(32),
+                  ),
+                  onPressed: () {},
+                ),
+              ),
+              Spacer(),
+              Transform.translate(
+                offset: Offset(32 * offset, 0),
+                child: Text(
+                  '0.00 \$',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+              SizedBox(width: 16),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+
