ea97f09bc848aa10514da244a2b2cf89eaaf893a
namld <17520777@gm.uit.edu.vn>
2020-12-18 16:12:12 +0700

    Update CommunityScreen.dart

--- lib/Screens/CommunityScreen.dart
+++ lib/Screens/CommunityScreen.dart
@@ -1,10 +1,163 @@
 import 'package:flutter/material.dart';
 
-class CommunityScreen extends StatelessWidget{
+class CommunityScreen extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+  _CommunityScreenState createState() => _CommunityScreenState();
+}
+
+class _CommunityScreenState extends State<CommunityScreen> {
+
+
+  PageController _pageController;
+  int _page = 0;
+  List navs = ["Trang chủ", "Cộng đồng", "Yêu thích"];
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Business',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Container(
+            margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 8.0),
+            decoration: BoxDecoration(
+              color: Color.fromARGB(50, 255, 255, 255),
+              borderRadius: BorderRadius.all(Radius.circular(22.0)),
+            ),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Expanded(
+                    flex: 1,
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Địa điểm muốn khám phá",
+                        hintStyle: TextStyle(color: Colors.white),
+                        icon: Icon(Icons.search, color: Colors.white),
+                      ),
+                    )),
+                Expanded(
+                    flex: 0,
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.more_vert, color: Colors.white),
+                      padding: EdgeInsets.symmetric(horizontal: 5.0),
+                    ))
+              ],
+            )),
+        backgroundColor: Colors.orange,
+      ),
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context,index)=>CardPost(),
+        ),
+
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
+class CardPost extends StatelessWidget{
+    const CardPost({
+      Key key,
+}): super (key: key);
+    @override
+  Widget build(BuildContext context) {
+      return Card(
+        child: Container(
+          height: 350.0,
+          child: Column(
+            children: <Widget>[
+              ListTile(
+                leading: CircleAvatar(),
+                title: Text("Investor Tactics Developer"),
+                subtitle: Text(
+                    "Thứ năm lúc 21:39"
+                ),),
+              Expanded(
+                child: Container(
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: NetworkImage(
+                              "https://dulich9.com/wp-content/uploads/2019/05/bien-sam-son-9.jpg"),
+                          fit: BoxFit.cover,
+                        )
+                    )
+                ),
+              ),
+              SizedBox(height: 14.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    Row(
+
+                        children: <Widget>[
+                          Icon(Icons.thumb_up, color: Colors.grey),
+                          SizedBox(width: 8.0),
+                          Text("Thích", style: TextStyle(color: Colors.grey))
+                        ]
+                    ),
+                    Row(
+                        children: <Widget>[
+                          Icon(Icons.comment, color: Colors.grey),
+                          SizedBox(width: 8.0),
+                          Text(
+                              "Bình luận", style: TextStyle(color: Colors.grey))
+                        ]
+                    ),
+                    Row(
+                        children: <Widget>[
+                          Icon(Icons.share, color: Colors.grey),
+                          SizedBox(width: 8.0),
+                          Text("Chia sẻ", style: TextStyle(color: Colors.grey))
+                        ]
+                    )
+                  ]
+              ),
+              SizedBox(height: 12.0,)
+            ],
+          ),
+        ),
+      );
+    }
 }
